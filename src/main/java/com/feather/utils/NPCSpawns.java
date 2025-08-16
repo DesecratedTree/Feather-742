5202349cef1c6c275c8bfa08146cc823f75cdbbf
DesecratedTree <52549792+DesecratedTree@users.noreply.github.com>
2025-08-16 02:44:11 -0500

    fix: uploaded wrong npcspawns

--- src/main/java/com/feather/utils/NPCSpawns.java
+++ src/main/java/com/feather/utils/NPCSpawns.java
@@ -1,111 +1,175 @@
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//  Copyright (C) 2021 Trenton Kress
-//  This file is part of project: Darkan
-//
-package com.rs.utils.spawns;
+package com.feather.utils;
 
-import com.google.gson.JsonIOException;
-import com.rs.cache.loaders.NPCDefinitions;
-import com.rs.game.World;
-import com.rs.lib.file.JsonFileManager;
-import com.rs.lib.game.Tile;
-import com.rs.lib.util.Logger;
-import com.rs.plugin.annotations.PluginEventHandler;
-import com.rs.plugin.annotations.ServerStartupEvent;
-import com.rs.plugin.annotations.ServerStartupEvent.Priority;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@PluginEventHandler
+import com.feather.cache.parser.NPCDefinitions;
+import com.feather.game.World;
+import com.feather.game.WorldTile;
+import com.feather.game.npc.NPC;
+
 public final class NPCSpawns {
 
-	private final static String PATH = "data/npcs/spawns/";
-	final static Charset ENCODING = StandardCharsets.UTF_8;
-
 	private static final Object lock = new Object();
-	private static final ArrayList<NPCSpawn> ALL_SPAWNS = new ArrayList<>();
-	private static final ArrayList<NPCSpawn> ADDED_SPAWNS = new ArrayList<>();
-	private static final Map<Integer, List<NPCSpawn>> NPC_SPAWNS = new HashMap<>();
-
-	public static boolean addSpawn(String username, int id, Tile tile) {
-		synchronized (lock) {
-			File file = new File("./data/npcs/addedSpawns.json");
-			ADDED_SPAWNS.add(new NPCSpawn(id, tile, ""+NPCDefinitions.getDefs(id).getName()+" added by " + username));
+	public static boolean addSpawn(String username, int id, WorldTile tile) throws Throwable {
+		synchronized(lock) {
+			File file = new File("data/npcs/spawns.txt");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+			writer.write("// "+NPCDefinitions.getNPCDefinitions(id).name+", "+NPCDefinitions.getNPCDefinitions(id).combatLevel+", added by: "+username);
+			writer.newLine();
+			writer.flush();
+			writer.write(id+" - "+tile.getX()+" "+tile.getY()+" "+tile.getPlane());
+			writer.newLine();
+			writer.flush();
 			World.spawnNPC(id, tile, -1, true);
-			try {
-				JsonFileManager.saveJsonFile(ADDED_SPAWNS, file);
-			} catch (IOException e) {
-				return false;
+			return true;
+		}
+
+	}
+
+	public static boolean removeSpawn(NPC npc) throws Throwable {
+		synchronized(lock) {
+			List<String> page = new ArrayList<>();
+			File file = new File("data/npcs/spawns.txt");
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			String line;
+			boolean removed = false;
+			int id =  npc.getId();
+			WorldTile tile = npc.getRespawnTile();
+			while((line = in.readLine()) != null)  {
+				if(line.equals(id+" - "+tile.getX()+" "+tile.getY()+" "+tile.getPlane())) {
+					page.remove(page.get(page.size()-1)); //description
+					removed = true;
+					continue;
+				}
+				page.add(line);
 			}
+			if(!removed)
+				return false;
+			file.delete();
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			for(String l : page) {
+				writer.write(l);
+				writer.newLine();
+				writer.flush();
+			}
+			npc.finish();
 			return true;
 		}
 	}
 
-	@ServerStartupEvent(Priority.FILE_IO)
-	public static final void init() throws JsonIOException, IOException {
-		Logger.info(NPCSpawns.class, "init", "Loading NPC spawns...");
-		File[] spawnFiles = new File(PATH).listFiles();
-		for (File f : spawnFiles)
-			load(f);
-		Logger.info(NPCSpawns.class, "init", "Loaded " + ALL_SPAWNS.size() + " NPC spawns...");
+	public static final void init() {
+		if (!new File("data/npcs/packedSpawns").exists())
+			packNPCSpawns();
 	}
 
-	public static void load(File file) throws IOException {
-		if (file.getName().startsWith("_"))
-			return;
-		if (file.isDirectory()) {
-			for (File f : file.listFiles())
-				load(f);
-			return;
+	private static final void packNPCSpawns() {
+		Logger.log("NPCSpawns", "Packing npc spawns...");
+		if (!new File("data/npcs/packedSpawns").mkdir())
+			throw new RuntimeException(
+					"Couldn't create packedSpawns directory.");
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(
+					"data/npcs/unpackedSpawnsList.txt"));
+			while (true) {
+				String line = in.readLine();
+				if (line == null)
+					break;
+				if (line.startsWith("//"))
+					continue;
+				String[] splitedLine = line.split(" - ", 2);
+				if (splitedLine.length != 2)
+					throw new RuntimeException("Invalid NPC Spawn line: "
+							+ line);
+				int npcId = Integer.parseInt(splitedLine[0]);
+				String[] splitedLine2 = splitedLine[1].split(" ", 5);
+				if (splitedLine2.length != 3 && splitedLine2.length != 5)
+					throw new RuntimeException("Invalid NPC Spawn line: "
+							+ line);
+				WorldTile tile = new WorldTile(
+						Integer.parseInt(splitedLine2[0]),
+						Integer.parseInt(splitedLine2[1]),
+						Integer.parseInt(splitedLine2[2]));
+				int mapAreaNameHash = -1;
+				boolean canBeAttackFromOutOfArea = true;
+				if (splitedLine2.length == 5) {
+					mapAreaNameHash = Utils.getNameHash(splitedLine2[3]);
+					canBeAttackFromOutOfArea = Boolean
+							.parseBoolean(splitedLine2[4]);
+				}
+				addNPCSpawn(npcId, tile.getRegionId(), tile, mapAreaNameHash,
+						canBeAttackFromOutOfArea);
+			}
+			in.close();
+		} catch (Throwable e) {
+			Logger.handle(e);
 		}
-		NPCSpawn[] spawns = (NPCSpawn[]) JsonFileManager.loadJsonFile(file, NPCSpawn[].class);
-		if (spawns != null)
-			for(NPCSpawn spawn : spawns)
-				add(spawn);
 	}
 
-	public static void add(NPCSpawn spawn) {
-		if (spawn != null) {
-			ALL_SPAWNS.add(spawn);
-			List<NPCSpawn> regionSpawns = NPC_SPAWNS.get(spawn.getTile().getChunkId());
-			if (regionSpawns == null)
-				regionSpawns = new ArrayList<>();
-			regionSpawns.add(spawn);
-			NPC_SPAWNS.put(spawn.getTile().getChunkId(), regionSpawns);
+	public static final void loadNPCSpawns(int regionId) {
+		File file = new File("data/npcs/packedSpawns/" + regionId + ".ns");
+		if (!file.exists())
+			return;
+		try {
+			RandomAccessFile in = new RandomAccessFile(file, "r");
+			FileChannel channel = in.getChannel();
+			ByteBuffer buffer = channel.map(MapMode.READ_ONLY, 0,
+					channel.size());
+			while (buffer.hasRemaining()) {
+				int npcId = buffer.getShort() & 0xffff;
+				int plane = buffer.get() & 0xff;
+				int x = buffer.getShort() & 0xffff;
+				int y = buffer.getShort() & 0xffff;
+				boolean hashExtraInformation = buffer.get() == 1;
+				int mapAreaNameHash = -1;
+				boolean canBeAttackFromOutOfArea = true;
+				if (hashExtraInformation) {
+					mapAreaNameHash = buffer.getInt();
+					canBeAttackFromOutOfArea = buffer.get() == 1;
+				}
+				World.spawnNPC(npcId, new WorldTile(x, y, plane),
+						mapAreaNameHash, canBeAttackFromOutOfArea);
+			}
+			channel.close();
+			in.close();
+		} catch (Throwable e) {
+			Logger.handle(e);
 		}
 	}
 
-	public static List<NPCSpawn> getAllSpawns() {
-		return ALL_SPAWNS;
+	private static final void addNPCSpawn(int npcId, int regionId,
+										  WorldTile tile, int mapAreaNameHash,
+										  boolean canBeAttackFromOutOfArea) {
+		try {
+			DataOutputStream out = new DataOutputStream(new FileOutputStream(
+					"data/npcs/packedSpawns/" + regionId + ".ns", true));
+			out.writeShort(npcId);
+			out.writeByte(tile.getPlane());
+			out.writeShort(tile.getX());
+			out.writeShort(tile.getY());
+			out.writeBoolean(mapAreaNameHash != -1);
+			if (mapAreaNameHash != -1) {
+				out.writeInt(mapAreaNameHash);
+				out.writeBoolean(canBeAttackFromOutOfArea);
+			}
+			out.flush();
+			out.close();
+		} catch (Throwable e) {
+			Logger.handle(e);
+		}
 	}
 
-	public static void loadNPCSpawns(int chunkId) {
-		List<NPCSpawn> spawns = NPC_SPAWNS.get(chunkId);
-		if (spawns != null)
-			for (NPCSpawn spawn : spawns)
-				spawn.spawn();
-	}
-
-	public static List<NPCSpawn> getSpawnsForChunk(int chunkId) {
-		return NPC_SPAWNS.get(chunkId);
+	private NPCSpawns() {
 	}
 }
